2106850dd226736fa85e548f93d9741812a137f5
Ryan Peh <ryanpxp@outlook.com>
2023-03-01 17:07:34 +0800

    navbar1.3

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,4 @@
-import Image from 'next/image'
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import styles from '../styles/Navbar.module.css'
-import Button from '../components/Button'
+import Link from "next/link"
 
 interface props {
     username: string
@@ -11,17 +6,32 @@ interface props {
 
 const Navbar = (props: props) => {
     return (
-        <div className='bg-primary drop-shadow-md flex items-center justify-between p-4 h-full'>
-            <div className='flex flex-col justify-center'>
-                
+        <div className='bg-primary flex items-center justify-between p-4 drop-shadow-md w-full'>
+            <div className='flex justify-center gap-10'>
                 <img src="/logo.png" alt="Logo" className="h-12 invert"></img>
-                
+                <div className='flex gap-10 p-4'>
+                    <Link href="#" className="text-secondary font-semibold">Category</Link>
+                    <Link href="#" className="text-secondary font-semibold">FAQ</Link>
+                </div>    
             </div>
-            
-            <div className='flex h-full c-full gap-4'>
-                    <a href="#" className="text-secondary">Create event</a>
-                    <a href="#" className="text-secondary">Sign in</a>
-                    <a href="#" className="text-secondary">{props.username}</a>
+
+            <form className='flex w-1/3 justify-between'>
+                    
+                    <input type="text" id="search-bar" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 w-full" placeholder="Search" required/>
+
+                    <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800">
+                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
+                            </path>
+                        </svg>
+                        <span className="sr-only">Search</span>
+                    </button>
+                
+            </form>
+
+            <div className='flex gap-4'>
+                <Link href="#" className="text-secondary font-semibold">Create event</Link>
+                <Link href="#" className="text-secondary font-semibold">{props.username}</Link>
             </div>
         </div>
     )
