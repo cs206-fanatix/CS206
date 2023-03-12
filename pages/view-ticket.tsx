ddf05f9997266ff6012accb07e921342e3c65198
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2023-03-12 23:06:00 +0800

    fix build

--- pages/view-ticket.tsx
+++ pages/view-ticket.tsx
@@ -2,9 +2,63 @@ import type { NextPage } from 'next';
 import { useState } from 'react';
 import Image from 'next/image';
 import EventCard from '../components/EventCard';
-import data from '../pages/event';
 
 const viewTicket: NextPage = () => {
+	const data = [{
+        id:1,
+        title: "Born Pink World Tour",
+        artist: "Blackpink",
+        imageURL: "/static/images/bp.jpg",
+        dateTime: "9/3/2023",
+        venue: "Singapore indoor stadium",
+        status: "listed"
+
+    },
+    {
+        id:2,
+        title: "Born Pink World Tour",
+        artist: "Blackpink",
+        imageURL: "/static/images/bp.jpg",
+        dateTime: "9/3/2023",
+        venue: "Singapore indoor stadium",
+        status: "unsold"
+    },
+    {
+        id:3,
+        title: "Born Pink World Tour",
+        artist: "Blackpink",
+        imageURL: "/static/images/bp.jpg",
+        dateTime: "9/3/2023",
+        venue: "Singapore indoor stadium",
+        status: "listed"
+    },
+    {
+        id:4,
+        title: "Born Pink World Tour",
+        artist: "Blackpink",
+        imageURL: "/static/images/bp.jpg",
+        dateTime: "9/3/2023",
+        venue: "Singapore indoor stadium",
+        status: "listed"
+    },
+    {
+        id:5,
+        title: "Born Pink World Tour",
+        artist: "Blackpink",
+        imageURL: "/static/images/bp.jpg",
+        dateTime: "9/3/2023",
+        venue: "Singapore indoor stadium",
+        status: "unsold"
+    },
+    {
+        id:6,
+        title: "Born Pink World Tour",
+        artist: "Blackpink",
+        imageURL: "/static/images/bp.jpg",
+        dateTime: "9/3/2023",
+        venue: "Singapore indoor stadium",
+        status: "unsold"
+    }]
 	const cards = data.map((item) => {
 		return (
 			<EventCard
