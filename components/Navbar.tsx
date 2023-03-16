3adb947474f1532f9eb5197d85fd76ed7df49903
Jonathan Chua <jonathan.chuazh@seamoney.com>
2023-03-16 12:09:11 +0800

    update navbar logic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,23 +4,24 @@ import Image from "next/image";
 
 interface props {
   username: string;
+  userId?: string;
 }
 
 const Navbar = (props: props) => {
   const [open, setOpen] = useState(false);
-  
+  const [user, setUser] = useState(null);
+
   const menuRef = useRef<HTMLDivElement>(null);
   const imgRef = useRef<HTMLDivElement>(null);
 
-  /*
-  useEffect(()=>{
-    window.addEventListener("click", (e) => {
-      if (e.target !== menuRef.current && e.target !== imgRef.current){
-        setOpen(false);
-      }
-    });
-  })
- */
+  useEffect(() => {
+    // window.addEventListener("click", (e) => {
+    //   if (e.target !== menuRef.current && e.target !== imgRef.current){
+    //     setOpen(false);
+    //   }
+    // });
+  }, []);
+
   function toggleDropdown() {
     setOpen(() => !open);
   }
@@ -29,9 +30,14 @@ const Navbar = (props: props) => {
     <nav className="items-center w-screen sticky top-0 z-50">
       <div className="bg-primary flex p-4 drop-shadow-md w-full">
         <div className="flex gap-10 w-full">
-        <div className="flex cursor-pointer align-auto">
+          <div className="flex cursor-pointer align-auto">
             <Link href="/" passHref>
-              <Image src="/static/images/logo.png" alt="Logo" width={140} height={40} />
+              <Image
+                src="/static/images/logo.png"
+                alt="Logo"
+                width={140}
+                height={40}
+              />
             </Link>
           </div>
           <div className="flex gap-10 p-4">
@@ -45,7 +51,7 @@ const Navbar = (props: props) => {
           <form className="flex h-2/3 mb-auto mt-auto w-[40%]">
             <div className="flex relative w-full">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <span className="material-symbols-outlined">search</span>
+                <span className="material-symbols-outlined">search</span>
               </div>
               <input
                 type="text"
@@ -58,30 +64,62 @@ const Navbar = (props: props) => {
           </form>
         </div>
 
+        <div className="px-4 flex gap-4 items-center relative">
+          {/* display login and signup button if user is not logged in */}
 
-        
+          {user == null && (
+            <Link href="/login" passHref>
+              <a className="whitespace-nowrap w-auto items-center px-3 py-2 text-sm font-medium text-center text-white bg-accent rounded-lg hover:bg-red-900">
+                Log In
+              </a>
+            </Link>
+          )}
 
-        <div className="pr-4 flex gap-4 items-center relative">
-          <Link href="#" passHref className="text-secondary font-semibold cursor-pointer">
-            <span className="material-symbols-outlined">shopping_cart</span>
-          </Link>
-          <div ref={imgRef}>
-          <Image src="/static/images/profile.png" alt="Profile" width={40} height={40} className="rounded-full cursor-pointer" onClick={toggleDropdown} />
-          </div>
+          {user == null && (
+            <Link href="/signup" passHref>
+              <a className="whitespace-nowrap w-auto items-center px-3 py-2 text-sm font-medium text-center text-white bg-accent rounded-lg hover:bg-red-900">
+                Sign Up
+              </a>
+            </Link>
+          )}
 
+          {user != null && (
+            <Link
+              href="#"
+              passHref
+              className="text-secondary font-semibold cursor-pointer"
+            >
+              <span className="material-symbols-outlined">shopping_cart</span>
+            </Link>
+          )}
+          {user != null && (
+            <div ref={imgRef}>
+              <Image
+                src="/static/images/profile.png"
+                alt="Profile"
+                width={40}
+                height={40}
+                className="rounded-full cursor-pointer"
+                onClick={toggleDropdown}
+              />
+            </div>
+          )}
           {open && (
-            <div ref={menuRef} className="bg-white p-2 w-40 shadow-lg absolute top-9 right-3">
+            <div
+              ref={menuRef}
+              className="bg-white p-2 w-40 shadow-lg absolute top-9 right-3"
+            >
               <h3>{props.username}</h3>
               <hr></hr>
               <ul>
-                <Link href="/view-ticket">
-                <li
-                  className="p-1 text-xs cursor-pointer rounded hover:bg-accent"
-                  key="View Tickets"
-                  onClick={toggleDropdown}
-                >
-                  View Tickets
-                </li>
+                <Link href="/view-ticket" passHref>
+                  <li
+                    className="p-1 text-xs cursor-pointer rounded hover:bg-accent"
+                    key="View Tickets"
+                    onClick={toggleDropdown}
+                  >
+                    View Tickets
+                  </li>
                 </Link>
                 <li
                   className="p-1 text-xs cursor-pointer rounded hover:bg-accent"
