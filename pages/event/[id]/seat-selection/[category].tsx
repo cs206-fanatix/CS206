c35030c682e064ffa3f605878f5d137ca884de45
Ryan Peh <ryanpxp@outlook.com>
2023-03-28 19:19:57 +0800

    error message

--- pages/event/[id]/seat-selection/[category].tsx
+++ pages/event/[id]/seat-selection/[category].tsx
@@ -308,7 +308,7 @@ const SeatSelect: NextPage = () => {
         return (
             <div className="flex flex-col items-center justify-center gap-2 h-120 max-w-4xl">
                 <p className='text-red-500 text-xl font-semibold'>Error {HTTPStatus}</p>
-                <p className='text-secondary text-lg font-semibold'>Something went wrong...</p>
+                <p className='text-secondary text-lg font-semibold'>Something went wrong... Try refreshing</p>
             </div>
         )
     }
